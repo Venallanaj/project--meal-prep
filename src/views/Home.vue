9a2ee9fb94183e9f34272e259985a492bb4ebb13
Vena <venetike.llanaj@hideolab.al>
2022-03-29 14:16:37 +0200

    add changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,25 @@
 <template>
-  <hello-world />
+   <span>
+        <home-hero></home-hero>
+        <home-details></home-details>
+        <home-plans></home-plans>
+   </span>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+//import HelloWorld from '../components/HelloWorld';
+import HomeHero from '@/components/HomeHero';
+import HomeDetails from '@/components/HomeDetails.vue';
+import HomePlans from '@/components/HomePlans';
+
+
 
 export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
-  },
+    name: 'Home',
+    components: {
+    HomeHero,
+    HomeDetails,
+    HomePlans,
+    },
 };
 </script>
